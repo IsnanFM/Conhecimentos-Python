e91ec561263575194f2fef4ba552a522969eb5a7
IsnanFM <isnanmota@gmail.com>
2023-08-20 20:33:43 -0300

    tipos_dados

---
+++ Tipos de dados/tipos.py
@@ -0,0 +1,15 @@
+#principais tipos de dados utilizados
+
+i = 12
+f= 12.2
+s = "esse é um tipo string"#uma sequencia de caracteres: string
+lst = [1,2,3,4,5,6] #uma lista qualquer, podendo ser de caracteres, string ou numeros
+tpl = (1,4,3,1,4) #imutavel
+col = {1,3,4,5,6,2} #imutavel e os valores não se repetem
+
+print(i) # printando inteiro
+print(f) #flutuante
+print(s) #string
+print(lst)#lista
+print(tpl)#tupla
+print(col)#coleção
