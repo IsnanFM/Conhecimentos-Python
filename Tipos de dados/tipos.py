8e204283f96b27a607e18e96b8d2fe5cb6e81bb3
Isnan Ferreira <142614697+IsnanFM@users.noreply.github.com>
2023-08-20 22:38:39 -0300

    Update tipos.py

--- Tipos de dados/tipos.py
+++ Tipos de dados/tipos.py
@@ -1,11 +1,11 @@
-#principais tipos de dados utilizados
+#==============================================PRINCIPAIS TIPOS DE DADOS UTILIZADOS=======================================
 
-i = 12
-f = 12.2
-s = "esse é um tipo string"#uma sequencia de caracteres: string
-lst = [1,2,3,4,5,6] #uma lista qualquer, podendo ser de caracteres, string ou numeros
-tpl = (1,4,3,1,4) #imutavel
-col = {1,3,4,5,6,2} #imutavel e os valores não se repetem
+i = 12 #INTEIRO
+f = 12.2 #FLUTUANTE
+s = "esse é um tipo string"#uma sequencia de caracteres: STRING
+lst = [1,2,3,4,5,6] #uma LISTA qualquer, podendo ser de caracteres, string ou numeros
+tpl = (1,4,3,1,4) #imutavel (TUPLA)
+col = {1,3,4,5,6,2} #imutavel e os valores não se repetem (COLEÇÃO)
 
 print(i) # printando inteiro
 print(f) #flutuante
@@ -13,4 +13,3 @@ print(s) #string
 print(lst)#lista
 print(tpl)#tupla
 print(col)#coleção
-print(help())
